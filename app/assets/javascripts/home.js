052746150d72f2374e41c7ce0346067fa4e45e52
psave <paulsave01@gmail.com>
2016-05-04 20:38:53 -0700

    Adjusting date on homepage

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,16 +1,20 @@
-$(function() {
-  var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
-  $.getJSON( flickerAPI, {
-    tags: "puerto vallarta",
-    tagmode: "any",
-    format: "json"
-  })
-    .done(function( data ) {
-      $.each( data.items, function( i, item ) {
-        $( "<img>" ).attr( "src", item.media.m ).appendTo( "#pv-flicker-images" );
-        if ( i === 3 ) {
-          return false;
-        }
-      });
-    });
-})();
+// $(function() {
+//   var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+//   $.getJSON( flickerAPI, {
+//     tags: "puerto vallarta sunset",
+//     tagmode: "any",
+//     format: "json"
+//   })
+//     .done(function( data ) {
+//       $.each( data.items, function( i, item ) {
+//         $( "<img>" ).attr( "src", item.media.m ).appendTo( "#pv-flicker-images" );
+//         if ( i === 3 ) {
+//           return false;
+//         }
+//       });
+//     });
+// })();
+
+// Add the following to the home page
+// <div id="pv-flicker-images">
+// </div> <!-- pv flicker images -->
